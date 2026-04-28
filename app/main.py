ec0f16e724da5c2cd8aa05f07b589aba52d458f4
biren067 <birendrabitian@gmail.com>
2026-04-28 22:19:50 +0530

    added middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.models.input_model import InputModel
 from app.pipeline.rag_pipeline import build_rag_chain
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# ✅ Add CORS middleware here
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],  # allow all (good for development)
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 # Load once at startup
 rag_chain = build_rag_chain()
 
